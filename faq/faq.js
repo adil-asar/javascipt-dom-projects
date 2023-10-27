d26bb6303f0b096b011a2eb2b45243768f8a2dd3
Adil <react631@gmail.com>
2023-10-27 04:19:38 -0700

    updated

--- faq/faq.js
+++ faq/faq.js
@@ -1,16 +1,9 @@
 
-const question1 = document.getElementById('question1')
-const question2 = document.getElementById('question2')
-const question3 = document.getElementById('question3')
+const btns = document.querySelectorAll('.x-btn');
 
-function textManager1() {
-    question1.classList.toggle('show-text')
-}
-
-function textManager2() {
-    question2.classList.toggle('show-text')
-}
-
-function textManager3() {
-    question3.classList.toggle('show-text')
-}
+btns.forEach(function (btn) {
+    btn.addEventListener("click",function (e) {
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text')
+    })
+})
