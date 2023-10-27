89882567f24e419cb16d8d2b80b8d7b8db24d627
Adil <react631@gmail.com>
2023-10-27 04:02:27 -0700

    faq added

---
+++ faq/faq.js
@@ -0,0 +1,16 @@
+
+const question1 = document.getElementById('question1')
+const question2 = document.getElementById('question2')
+const question3 = document.getElementById('question3')
+
+function textManager1() {
+    question1.classList.toggle('show-text')
+}
+
+function textManager2() {
+    question2.classList.toggle('show-text')
+}
+
+function textManager3() {
+    question3.classList.toggle('show-text')
+}
