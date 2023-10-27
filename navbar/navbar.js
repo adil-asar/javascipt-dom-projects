5e6dc177acb87d14ab20006b09a59ed546059fd2
Adil <react631@gmail.com>
2023-10-27 00:57:42 -0700

    navbar added

---
+++ navbar/navbar.js
@@ -0,0 +1,9 @@
+
+
+    
+const menubtn = document.querySelector('.menu-button');
+const forMobile = document.querySelector('#forMobile')
+
+function ToggleMenu(){
+forMobile.classList.toggle('nav-display-block')
+}
